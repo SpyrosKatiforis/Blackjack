627f1f544d5c242b0417799d95595abfb41ffe6f
SpyrosK <57183108+SpyrosKatiforis@users.noreply.github.com>
2022-03-11 17:36:51 +0200

    Add files via upload

---
+++ Blackjack-main/blackjack.py
@@ -0,0 +1,65 @@
+from deck import Deck
+from player import Player
+
+wins = 0
+losses = 0
+
+
+class Blackjack:
+    def __init__(self):
+        # create deck instance variable
+        self.deck = Deck()
+        # call deck's generate function
+        self.deck.generate()
+        # create player
+        self.player = Player(False, self.deck)
+        # create dealer
+        self.dealer = Player(True, self.deck)
+
+    def play(self, num):
+        global wins, losses
+        player_status = self.player.deal()
+        dealer_status = self.dealer.deal()
+
+        if player_status == 1:
+            # if player draws 21 first try
+            wins += 1
+            return
+
+        if dealer_status == 1:
+            # if dealer draws 21 first try
+            losses += 1
+            return
+
+        while self.player.score < num:
+            # if player's score is below num then we add
+            bust = 0
+
+            if self.player.score < num:
+                bust = self.player.hit()
+
+            # if player score > 21
+            if bust == 1:
+                losses += 1
+                return
+
+        if dealer_status == 1:
+            # if dealer's cards have value == 21
+            losses += 1
+            return
+
+        while self.dealer.check_score() < 17:
+            # dealer must have at least 17 card value according to the rules
+            if self.dealer.hit() == 1:
+                # if dealer's cards have value > 21 then we win
+                wins += 1
+                return
+
+        if self.dealer.check_score() == self.player.check_score():
+            losses += 1
+
+        elif self.dealer.check_score() > self.player.check_score():
+            losses += 1
+
+        elif self.dealer.check_score() < self.player.check_score():
+            wins += 1
---
+++ Blackjack-main/main.py
@@ -0,0 +1,52 @@
+
+import re
+import blackjack
+from blackjack import Blackjack
+
+
+def print_wins_and_losses(max_number):
+    for _ in range(num_loops):
+        Blackjack().play(max_number)
+
+    print(f'Wins: {blackjack.wins} | Losses: {blackjack.losses}')
+    print(f'Wins: {blackjack.wins / num_loops * 100:.2f}% | Losses: {blackjack.losses / num_loops * 100:.2f}%')
+
+    print('\n\n')
+
+
+if __name__ == '__main__':
+    try:
+        while True:
+            num_loops = int(input('How many times you want to play? Press Enter for 10: '))
+            if num_loops > 0:
+                break
+    except ValueError:
+        print('Entering default value (10) for times to play\n')
+        num_loops = 10
+
+    try:
+        while True:
+            max_number = int(input('Maximum card value to Stand? Press Enter for 17: '))
+            if 0 < max_number <= 21:
+                break
+    except ValueError:
+        print('Entering default value (17) for maximum Stand\n')
+        max_number = 17
+
+    while True:
+        answer = input('Want to raise card Stand with each loop? [(y)es/n(o)] ').lower().strip()
+        # regular expression to catch y/yes or n/no only
+        if re.search(r'^(y(es)?|n(o)?)$', answer):
+            break
+    print('\n\n')
+
+    if re.search(r'^y(es)?$', answer):
+        while max_number <= 21:
+            print(f'Max Stand is currently: {max_number}')
+            print_wins_and_losses(max_number)
+            max_number += 1
+            # set to 0 for next iteration
+            blackjack.wins = 0
+            blackjack.losses = 0
+    else:
+        print_wins_and_losses(max_number)
